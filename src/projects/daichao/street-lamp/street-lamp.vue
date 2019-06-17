969c64de76f98cd4c82612b23b30d7580af971f3
MYQ1996 <954663633@qq.com>
2019-06-17 13:34:21 +0800

    修改部分

--- src/projects/daichao/street-lamp/street-lamp.vue
+++ src/projects/daichao/street-lamp/street-lamp.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="street-lamp-page fullscreen" v-cloak>
-        street-lamp
+        <input type="checkbox" checked="checked" class="toggle">
+        <div class="switch"></div>
+        <div class="light"></div>
     </div>
 </template>
     
